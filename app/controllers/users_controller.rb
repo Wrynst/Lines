90e5bdbb5202b31edfa3928f08c27ddd8a883700
Nathan Emerson Arnold <nathan.e.arnold@gmail.com>
2018-01-14 20:26:40 -0500

    Create user show action and add gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  
   def new
     @user = User.new
   end
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -10,6 +12,24 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
   
   private
